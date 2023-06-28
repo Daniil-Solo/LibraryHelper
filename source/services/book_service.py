976d9cd3213f7ee65757077ded4e40aab762947c
Daniil-Solo <daniil.solo1723@gmail.com>
2023-06-28 13:33:50 +0500

    add methods for genres in book-service

--- source/services/book_service.py
+++ source/services/book_service.py
@@ -2,6 +2,8 @@ import datetime
 from dataclasses import dataclass
 
 from services.base_service import BaseModelService
+from services.book_genre_service import BookGenreService, InBookGenre, OutBookGenre
+from services.genre_service import GenreService, OutGenre
 
 
 @dataclass
@@ -22,3 +24,16 @@ class BookService(BaseModelService):
 
     def get_default_fields(self) -> dict:
         return dict(register_date=datetime.date.today())
+
+    def get_genres(self, book_id: int) -> list[OutGenre]:
+        bgs = BookGenreService(self.conn)
+        return bgs.join(GenreService, 'genre_id', book_id=book_id)
+
+    def add_genre(self, book_id: int, genre_id: int) -> OutBookGenre:
+        bgs = BookGenreService(self.conn)
+        book_genre = bgs.create(InBookGenre(book_id=book_id, genre_id=genre_id))
+        return book_genre
+
+    def remove_genre(self, book_genre_id: int) -> None:
+        bgs = BookGenreService(self.conn)
+        bgs.delete(book_genre_id)
